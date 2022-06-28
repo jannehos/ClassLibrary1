26a6fbd76be737b94fca5f20545c5bc201946586
mariajosefina <mariatoivanen@outlook.com>
2022-06-28 10:35:53 +0300

    Initialized project under maria branch

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -2,7 +2,39 @@
 
 namespace ClassLibrary1
 {
-    public class Class1
+    public class DayCalculation
     {
+           
+
+    public bool IsFuture(DateTime CompletionDate)
+    {
+
+        DateTime Today = DateTime.Now;
+
+
+        if (CompletionDate > Today)
+        {
+
+            return true;
+
+
+        }
+        else if (CompletionDate < Today)
+        {
+            return false;
+        }
+
+        return false;
+
     }
+
 }
+
+
+    }
+
+
+
+
+    
+
